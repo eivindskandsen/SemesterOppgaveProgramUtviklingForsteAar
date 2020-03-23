0dc90b2f2a2db6c3a68a36d0978e9ee78dad4d68
Eivind <eivindskandsen@hotmail.no>
2020-03-23 13:04:27 +0100

    Satt opp litt gui og tableview

---
+++ src/sample/Controller.java
@@ -0,0 +1,33 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+
+
+public class Controller {
+    @FXML
+    private Label lblNavn;
+
+    @FXML
+    private Label lblDel;
+
+    @FXML
+    private Label lblPris;
+
+    @FXML
+    private TextField txtNavn;
+
+    @FXML
+    private TextField txtDel;
+
+    @FXML
+    private TextField txtPris;
+
+    @FXML
+    private Button buttonLeggTil;
+
+
+}
