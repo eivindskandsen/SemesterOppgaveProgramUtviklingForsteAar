087b9b3d848e678147ea687575c9fe7b65c8f855
Eivind <eivindskandsen@hotmail.no>
2020-03-24 13:37:00 +0100

    Fortsettelse av regex

--- src/sample/PcValidator.java
+++ src/sample/PcValidator.java
@@ -1,7 +1,11 @@
 package sample;
 
 public class PcValidator {
-    static boolean isValidName(String navn){
-if(navn.isEmpty() || navn.length() > 50 || )
+    static boolean isValidName(String navn) {
+        if (navn.isEmpty() || navn.length() > 50 || !Regex.regexName(navn)) {
+            return false;
+        }else{
+            return true;
+        }
     }
 }
--- src/sample/Regex.java
+++ src/sample/Regex.java
@@ -1,8 +1,11 @@
 package sample;
 
+import java.util.regex.Matcher;
+
 public class Regex {
     public static boolean regexName(String name) {
         String regexName = "[A-Za-z+_. ]+";
-        return name.mathces(regexName);
+
+        return name.matches(regexName);
     }
 }
