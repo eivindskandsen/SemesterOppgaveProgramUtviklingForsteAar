727daf8a5122ceab0a77e2abf681d9ced4ef3d55
Eivind <eivindskandsen@gotmail.no>
2020-04-08 12:21:51 +0200

    Added DataCollection

--- src/sample/DataCollection.java
+++ src/sample/DataCollection.java
@@ -2,11 +2,17 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
 
-import javax.swing.text.TableView;
 
 public class DataCollection {
     private ObservableList<Pc> list= FXCollections.observableArrayList();
 
-public void attachTableView(TableView tv) { tv.setItems(list)}
+public void attachTableView(TableView tv) { tv.setItems(list);}
+
+    public void addElement(Pc obj) {list.add(obj);}
+
+    public ObservableList<Pc> getList() {
+        return list;
     }
+}
